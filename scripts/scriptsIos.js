bbd75572a4babe68cb5cb9ff4cea719ba868787d
Guilherme Magnabosco <gwmagnabosco@inf.ufpel.edu.br>
2020-05-19 14:56:16 -0300

    adding ios section

--- scripts/scriptsIos.js
+++ scripts/scriptsIos.js
@@ -6,6 +6,7 @@ var img2 = document.getElementById("myImg2");
 var img3 = document.getElementById("myImg3");
 var img4 = document.getElementById("myImg4");
 var img5 = document.getElementById("myImg5");
+var img6 = document.getElementById("myImg6");
 
 var modalImg = document.getElementById("img01");
 
@@ -18,22 +19,26 @@ img.onclick = function open() {
   captionText.innerHTML = this.alt;
 }
 img2.onclick = function open() {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-  }
-img3.onclick = function open() {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
 }
-
+img3.onclick = function open() {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
 img4.onclick = function open() {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
 }
 img5.onclick = function open() {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img6.onclick = function open() {
   modal.style.display = "block";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
